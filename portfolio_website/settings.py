b1a2d687aec0a3658bd104e4dfc38c6a51736783
vinmutua <vincentmutua02@gmail.com>
2025-07-09 01:39:30 +0300

    Add static CSS and JS files

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -116,10 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / 'static',
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
